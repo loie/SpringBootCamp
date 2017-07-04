d42fe42707220af4b45dfce14d19eaac4405ada5
Lorenz Merdian <lorenz.merdian@googlemail.com>
2017-07-04 22:02:52 +0200

    Some slighty more complex test

--- src/test/kotlin/de/tutorials/springboot/ServerTest.kt
+++ src/test/kotlin/de/tutorials/springboot/ServerTest.kt
@@ -1,7 +1,11 @@
 package de.tutorials.springboot
 
+import de.tutorials.springboot.controller.GreetController
+import org.assertj.core.api.Assertions.assertThat
+
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
@@ -9,7 +13,12 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 public class ServerTest {
 
+    @Autowired
+    lateinit var controller : GreetController
+
+
     @Test
     fun contextLoads () {
+        assertThat(this.controller).isNotNull()
     }
 }
