85108675e68b486b6796dfaec4d25d4160bcf8fb
Lorenz Merdian <lorenz.merdian@googlemail.com>
2017-07-08 23:40:23 +0200

    Next step for postgres crud

---
+++ src/main/kotlin/de/tutorials/springboot/model/User.kt
@@ -0,0 +1,15 @@
+package de.tutorials.springboot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+data class User (
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
+    val name : String,
+    val email: String
+)
