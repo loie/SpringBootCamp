14a96de45cd429f956e97137d5566678e664db76
Lorenz Merdian <lorenz.merdian@googlemail.com>
2017-07-10 23:58:52 +0200

    just for today

---
+++ src/main/kotlin/de/tutorials/springboot/controller/MainController.kt
@@ -0,0 +1,36 @@
+package de.tutorials.springboot.controller
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.beans.factory.annotation.Autowired
+
+import de.tutorials.springboot.model.Customer
+import de.tutorials.springboot.repo.CustomerRepository
+
+@RestController
+class MainController {
+    @Autowired
+    lateinit var repository : CustomerRepository
+
+    @GetMapping("/save")
+    fun save() : String {
+        repository.save(Customer("Jack", "Smith"))
+        repository.save(Customer("Adam", "Johnson"))
+        repository.save(Customer("Kim", "Smith"))
+        repository.save(Customer("David", "Williams"))
+        repository.save(Customer("Peter", "Davis"))
+
+        return "done"
+    }
+
+    @GetMapping("/findall")
+    fun findAll() = repository.findAll()
+
+    @GetMapping("/findbyid/{id}")
+    fun findById(@PathVariable id: Long) = repository.findOne(id)
+
+    @GetMapping("/findbylastname/{lastName}")
+    fun findByLastName(@PathVariable lastName: String) = repository.findByLastName(lastName)
+}
