aa8af10948a9874659aa244d2c70f91c5c4a07ea
Lorenz Merdian <lorenz.merdian@googlemail.com>
2017-07-13 23:15:39 +0200

    added config

---
+++ src/main/kotlin/de/tutorials/springboot/MvcConfig.kt
@@ -0,0 +1,16 @@
+package de.tutorials.springboot
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+class MvcConfig : WebMvcConfigurerAdapter {
+    @Override
+    fun addViewControllers(registry: ViewControlleregistry) {
+        registry.addViewController("/home").setViewName("home")
+        registry.addViewController("/").setViewName("home")
+        registry.addViewController("/hello").setViewName("hello")
+        registry.addViewController("/login").setViewName("login")
+    }
+}
